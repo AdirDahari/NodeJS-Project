863d6d30d12f1df7f9abd29f7d9dbed9d7a3cdc8
AdirDahari <AdirDahari@users.noreply.github.com>
2024-01-05 23:50:53 +0200

    finish

--- src/validation/login.joi.ts
+++ src/validation/login.joi.ts
@@ -4,7 +4,7 @@ import { ILogin } from "../@types/user";
 
 const schema = Joi.object<ILogin>({
   email: Joi.string().email().min(5).max(100).required(),
-  password: Joi.string().pattern(passwordRegex).required(),
+  password: Joi.string().min(5).max(100).pattern(passwordRegex).required(),
 });
 
 export { schema as joiLoginSchema };
